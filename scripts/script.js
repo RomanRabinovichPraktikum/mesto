cfd83dc23d89f6e0282318fd7c54f6acd20b887f
Roman Rabinovich <romanrabinovich@yandex.ru>
2020-09-22 23:23:06 +0300

    feat: ESC press handler realized to close popup

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,17 @@ window.onload = function() {
     let profileSubTitle = document.querySelector('.profile__subtitle');
     
     let popup = document.querySelector('.popup');
-    let popupFirstNameField = popup.querySelector('.popup__first-name');
-    let popupPersonPosition = popup.querySelector('.popup__person-position');
+    let popupPersonField = popup.querySelector('.popup__text-field_role_person');
+    let popupPersonPositionField = popup.querySelector('.popup__text-field_role_position');
     let closeBtn = popup.querySelector('.popup__close-btn');
     let popupForm = popup.querySelector('.popup__form');
 
+    const checkPopupVisible = () => {
+        return popup.classList.contains('popup_opened');
+    }
+
     const togglePopupVisibility = () => {
-        if (popup.classList.contains('popup_opened'))
+        if (checkPopupVisible())
             popup.classList.remove('popup_opened');
         else
             popup.classList.add('popup_opened');
@@ -23,16 +27,31 @@ window.onload = function() {
 
     popupForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        profileTitle.textContent = popupFirstNameField.value;
-        profileSubTitle.textContent = popupPersonPosition.value;
+        profileTitle.textContent = popupPersonField.value;
+        profileSubTitle.textContent = popupPersonPositionField.value;
         togglePopupVisibility();
     });
 
 
     editBtn.addEventListener('click', (e) => {
-        popupFirstNameField.value = profileTitle.textContent;
-        popupPersonPosition.value = profileSubTitle.textContent;
+        popupPersonField.value = profileTitle.textContent;
+        popupPersonPositionField.value = profileSubTitle.textContent;
         togglePopupVisibility();
     });
 
+    //закрываем заодно и по ESC (удобно же ж)
+    document.addEventListener("keydown", (e) => {
+        e = e || window.event;
+        let isEscape = false;
+
+        if ("key" in e) {
+            isEscape = (e.key === "Escape" || e.key === "Esc");
+        } else {
+            isEscape = (e.keyCode === 27);
+        }
+        
+        if (isEscape && checkPopupVisible()) {
+            togglePopupVisibility();
+        }
+    });
 };
