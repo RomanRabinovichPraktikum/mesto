8461b2e4e37c6a04f3ce33f40c9822db91ffaf18
Roman Rabinovich <romanrabinovich@yandex.ru>
2020-12-12 20:55:21 +0300

    Section's items rendering changed

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,11 @@
 export default class Section {
-    constructor({ items, renderer }, containerSelector) {
-        this._items = items;
+    constructor(renderer, containerSelector) {
         this._renderer = renderer;
         this._containerSelector = containerSelector;
     }
 
-    renderItems() {
-        this._items.forEach((element) => {
+    renderItems(items) {
+        items.forEach((element) => {
             this._renderer(element);
         });
     }
